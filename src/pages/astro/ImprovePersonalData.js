933ce76450792592ba3e59ff424477871a3be9b6
meng028 <15701664107@163.com>
2018-10-22 09:25:23 +0800

    ss

--- src/pages/astro/ImprovePersonalData.js
+++ src/pages/astro/ImprovePersonalData.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {connect} from 'dva';
 import styles from './page.css';
 import {routerRedux} from 'dva/router';
+import {Button} from 'antd';
 
 import icon_man from "../../assets/astro/icon_man@2x.png";
 import icon_woman from "../../assets/astro/icon_woman@2x.png";
@@ -10,27 +11,17 @@ import icon_ziliao_xuanhao from "../../assets/astro/icon_ziliao_xuanhao@2x.png"
 import router from '../.umi/router';
 
 function ImprovePersonalData(props) {
-  console.log(1111111111111111111111,props.astro.selectAstro);
   return (
     <div>
       <div className={styles.improve_data}>
-          <div className={styles.improve_data_tit}>请完善个人资料</div>
-          <div className={styles.improve_data_content}>
-              <span className={styles.improve_data_content_sex}
-                    onClick={()=>{
-                      // this.setState({
-                      //   sex:"man"
-                      // })
-                    }}
-              >
-                <img src={icon_man} alt=""/>
-                <span>{props.astro.sex}</span>
-              </span>
-              <span className={styles.improve_data_content_sex}>
-                <img src={icon_woman} alt=""/>
-                <span>我是女生</span>
-              </span>
-          </div>
+        <div className={styles.improve_data_tit}>请完善个人资料</div>
+        <div className={styles.improve_data_content}>
+          <span className={styles.improve_data_content_sex} onClick={()=>{
+            console.log('addColor');
+
+          }}><img src={icon_man} alt=""/><span>我是男生</span></span>
+          <span className={styles.improve_data_content_sex}><img src={icon_woman} alt=""/><span>我是女生</span></span>
+        </div>
       </div>
       <div className={styles.improve_data}>
         <div className={styles.improve_data_tit}>我的星座</div>
@@ -41,12 +32,10 @@ function ImprovePersonalData(props) {
              }}
         >
            <span className={styles.improve_data_content_ziliao}>
-             <img className={styles.improve_data_content_ziliao_astro} src={bg_icon_shuangyu} alt=""/>
-             <span>{
-               // {props.astro.selectAstro}?'':{props.astro.selectAstro.cName}
-             }</span>
+             <img className={styles.improve_data_content_ziliao_astro} src={props.astro.selectAstro==null?'/static/icon_bg_shuangyu@2x.be7cbc3a.png':props.astro.selectAstro.img} alt=""/>
+             <span>{props.astro.selectAstro==null?"双鱼座":props.astro.selectAstro.cName}</span>
            </span>
-           <span><img className={styles.improve_data_content_ziliao_xuanhao} src={icon_ziliao_xuanhao} alt=""/></span>
+          <span><img className={styles.improve_data_content_ziliao_xuanhao} src={icon_ziliao_xuanhao} alt=""/></span>
         </div>
       </div>
       <div className={styles.improve_data_finished}>完成</div>
