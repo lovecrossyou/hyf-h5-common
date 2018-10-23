2adfefd0e65e5978cffdf727c05da9c24a4785ec
meng028 <15701664107@163.com>
2018-10-23 14:19:48 +0800

    星座

--- src/pages/astro/ImprovePersonalData.js
+++ src/pages/astro/ImprovePersonalData.js
@@ -5,21 +5,32 @@ import {routerRedux} from 'dva/router';
 import {Button} from 'antd';
 
 import icon_man from "../../assets/astro/icon_man@2x.png";
+import icon_man_h from "../../assets/astro/icon_man_h@2x.png";
 import icon_woman from "../../assets/astro/icon_woman@2x.png";
+import icon_woman_h from "../../assets/astro/icon_woman_h@2x.png";
 import bg_icon_shuangyu from "../../assets/astro/bg_icon_shuangyu@2x.png"
 import icon_ziliao_xuanhao from "../../assets/astro/icon_ziliao_xuanhao@2x.png"
 
 function ImprovePersonalData(props) {
+  const {sex} = props.astro ;
   return (
     <div>
       <div className={styles.improve_data}>
         <div className={styles.improve_data_tit}>请完善个人资料</div>
         <div className={styles.improve_data_content}>
-          <span className={styles.improve_data_content_sex} onClick={()=>{
-            console.log('addColor');
-
-          }}><img src={icon_man} alt=""/><span>我是男生</span></span>
-          <span className={styles.improve_data_content_sex}><img src={icon_woman} alt=""/><span>我是女生</span></span>
+          <span className={sex==='man' ? styles.improve_data_content_man:styles.improve_data_content_sex} onClick={()=>{
+            props.dispatch({
+              type:'astro/setSex',
+              payload:'man'
+            })
+          }}><img src={sex==='man' ? icon_man_h : icon_man} alt=""/><span>我是男生</span></span>
+          <span className={sex==="woman"?styles.improve_data_content_man:styles.improve_data_content_sex} onClick={()=>{
+              props.dispatch({
+                type:"astro/setSex",
+                payload:"woman"
+              })
+            }
+          }><img src={sex==="woman"?icon_woman_h:icon_woman} alt=""/><span>我是女生</span></span>
         </div>
       </div>
       <div className={styles.improve_data}>
