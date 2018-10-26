c81388205a6f90146e531db65b5d076b39dc0331
love1234 <345841634@qq.com>
2018-10-26 14:36:34 +0800

    完善地址新建

--- src/pages/wallet/page.js
+++ src/pages/wallet/page.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'dva';
 import { routerRedux } from 'dva/router';
-import { Toast} from 'antd-mobile';
+import { Toast,ActivityIndicator} from 'antd-mobile';
 
 import my_wallet_rmb_icon from '../../assets/my_wallet_rmb_icon.png';
 import my_wallet_xb_icon from '../../assets/my_wallet_xb_icon.png';
@@ -12,33 +12,43 @@ import me_icon_ticket_blue from '../../assets/me_icon_ticket_blue@2x.png';
 import me_icon_yinhangka from '../../assets/me_icon_yinhangka@2x.png';
 import styles from './page.css';
 
+const ContentPage = ({accountInfo,userProfitAllFriendInfo,userProfitInfo,dispatch})=>{
 
-class Wallet extends React.Component {
   // 我的好友
-  myFriend() {
-    this.props.dispatch(routerRedux.push('/friendlist/page'));
+  function myFriend() {
+    dispatch(routerRedux.push('/friendlist/page'));
   }
 
   // 邀请红包
-  inviteredPacket() {
-    this.props.dispatch(routerRedux.push('/inviteredpacket/page'));
+  function inviteredPacket() {
+    dispatch(routerRedux.push('/inviteredpacket/page'));
   }
 
   //喜币充值
-  xbCharge(){
+  function xbCharge(){
     Toast.info('请下载app使用!', 1);
   }
 
   // 会员奖励
-  vipReward(){
-    this.props.dispatch(routerRedux.push('/vipreward/page'));
+  function vipReward(){
+    dispatch(routerRedux.push('/vipreward/page'));
   }
 
+  // 点击运势
+  function astroClick(){
+    dispatch(routerRedux.push('/astro/page'));
+  };
 
-  render() {
-    const { accountInfo, userProfitAllFriendInfo, userProfitInfo } = this.props.store;
-    if (accountInfo == null) return null;
-    return <div>
+
+  // 会员中心
+  function vipClick(){
+    dispatch(routerRedux.push('/member/page'));
+  };
+
+
+  if (accountInfo == null) return null;
+  return (
+    <div>
       <div className={styles.wallet_header}>
         <div className={styles.wallet_item}>
           <div className='wallet_withdraw'>
@@ -57,21 +67,21 @@ class Wallet extends React.Component {
       </div>
       <div className={styles.wallet_fun}>
         <div className={styles.wallet_fun_item}>
-          <div className={styles.wallet_item_top} onClick={this.myFriend.bind(this)}>
+          <div className={styles.wallet_item_top} onClick={myFriend}>
             <img src={me_icon_shuipiao_haoyou} className={styles.my_friend_item_img}/>
             <div>我的好友</div>
           </div>
           <div className={styles.wallet_fun_count_item}>{userProfitAllFriendInfo.userProfitAllFriendAmount}人</div>
         </div>
         <div className={styles.wallet_fun_item}>
-          <div className={styles.wallet_item_top} onClick={this.inviteredPacket.bind(this)}>
+          <div className={styles.wallet_item_top} onClick={inviteredPacket}>
             <img src={me_icon_shuipiao_hongbao} className={styles.my_invite_item_img}/>
             <div>邀请红包</div>
           </div>
           <div className={styles.wallet_fun_count_item}>{userProfitInfo.inviteProfitXtbAmount}喜币</div>
         </div>
         <div className={styles.wallet_fun_item}>
-          <div className={styles.wallet_item_top} onClick={this.vipReward.bind(this)}>
+          <div className={styles.wallet_item_top} onClick={vipReward}>
             <img src={me_icon_shuipiao_jiangli} className={styles.my_sale_item_img}/>
             <div>会员奖励</div>
           </div>
@@ -80,7 +90,7 @@ class Wallet extends React.Component {
 
         <div className={styles.line}/>
 
-        <div onClick={this.xbCharge} className={styles.wallet_fun_item}>
+        <div onClick={xbCharge} className={styles.wallet_fun_item}>
           <div className={styles.wallet_item_top}>
             <img src={me_icon_ticket_blue} className={styles.my_water_ticket_item_img}/>
             <div>喜币充值</div>
@@ -95,14 +105,14 @@ class Wallet extends React.Component {
           <div className={styles.wallet_fun_count_item_big}>+</div>
         </div>
 
-        <div onClick={this.vipClick} className={styles.wallet_fun_item}>
+        <div onClick={vipClick} className={styles.wallet_fun_item}>
           <div className={styles.vip}>
             <img src={me_icon_ticket_blue} className={styles.my_water_ticket_item_img}/>
             <div>会员中心</div>
           </div>
         </div>
 
-        <div onClick={this.astroClick} className={styles.wallet_fun_item}>
+        <div onClick={astroClick} className={styles.wallet_fun_item}>
           <div className={styles.astro}>
             <img src={me_icon_ticket_blue} className={styles.my_water_ticket_item_img}/>
             <div>星座运势</div>
@@ -115,23 +125,31 @@ class Wallet extends React.Component {
           </div>
         </div>
       </div>
+    </div>
+  )
+}
+
+
+class Wallet extends React.Component {
+  render() {
+    const { accountInfo, userProfitAllFriendInfo, userProfitInfo } = this.props.store;
+    const {loading} = this.props ;
+    return <div>
+      <ContentPage
+        dispatch={this.props.dispatch}
+        userProfitAllFriendInfo={userProfitAllFriendInfo}
+        userProfitInfo={userProfitInfo}
+        accountInfo={accountInfo}/>
+      <ActivityIndicator
+        color="white"
+        toast
+        animating={loading}
+      />
     </div>;
   }
-
-
-  // 点击运势
-  astroClick = () => {
-    this.props.dispatch(routerRedux.push('/astro/page'));
-  };
-
-
-  // 会员中心
-  vipClick = () => {
-    this.props.dispatch(routerRedux.push('/member/page'));
-  };
-
 }
 
 export default connect(state => ({
   store: state.wallet,
+  loading: state.loading.global,
 }))(Wallet);
--- src/utils/authority.js
+++ src/utils/authority.js
@@ -1,7 +1,7 @@
 import config from './config';
 
 export function getAccessToken() {
-  if (config.isMock) return {"app_key":"b5958b665e0b4d8cae77d28e1ad3f521","signature":"D4D6D65A79E72A793B25A572C77B9191","access_token":"b649b1c01f0f467db6c3e80c7e9cf339"};
+  // if (config.isMock) return {"app_key":"b5958b665e0b4d8cae77d28e1ad3f521","signature":"D4D6D65A79E72A793B25A572C77B9191","access_token":"b649b1c01f0f467db6c3e80c7e9cf339"};
   return JSON.parse(localStorage.getItem('accessToken'));
 }
 
