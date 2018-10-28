4757e3d039682876cf7376d34d1ce15b3a7a48ad
lovecross <lovecrossyoutt@hotmail.com>
2018-10-28 15:44:10 +0800

    用户二维码完善

--- src/pages/personInfo/qrCode.js
+++ src/pages/personInfo/qrCode.js
@@ -5,17 +5,18 @@ import DocumentTitle from 'react-document-title';
 import styles from './page.css' ;
 
 function QrCode(props) {
-  const { qrData } = props.personInfo;
+  const { qrData,userInfo } = props.personInfo;
 
+  console.log('userInfo ', userInfo);
   console.log('qrData ', qrData);
   return <DocumentTitle title='我的二维码'>
     <div className={styles.qrcode}>
       <div className={styles.content}>
         <div className={styles.flex_r}>
-          <div className={styles.avatar}></div>
+          <img className={styles.avatar} src={userInfo.userInfo.icon} alt=''/>
           <div className={styles.info}>
-            <div className={styles.name}>猪猪侠9527</div>
-            <div className={styles.xt_number}>喜腾号：100008</div>
+            <div className={styles.name}>{userInfo.userInfo.cnName}</div>
+            <div className={styles.xt_number}>喜腾号：{userInfo.userInfo.xtNumber}</div>
           </div>
         </div>
         <div className={styles.img_border}>
