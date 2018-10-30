2656c30422660db3b42163700cfbcb421780cd91
love1234 <345841634@qq.com>
2018-10-30 16:45:35 +0800

    xx

--- .umirc.js
+++ .umirc.js
@@ -1,8 +1,8 @@
 export default {
   history: 'hash',
-  // publicPath:'./',
-  publicPath:'/xitenggamejar/dist/',
-  base:'/xitenggamejar/',
+  publicPath:'./',
+  // publicPath:'/xitenggamejar/dist/',
+  // base:'/xitenggamejar/',
   plugins: [
     ['umi-plugin-react', {
       antd: true,
@@ -13,9 +13,9 @@ export default {
   ],
   proxy: {
     "/api": {
-      target: "http://123.57.161.212:9939/xitenggamejar/",
+      // target: "http://123.57.161.212:9939/xitenggamejar/",
       // "target": "http://192.168.1.83:9939/xitenggamejar/",
-      // target: "https://www.xiteng.com/xitenggamejar/",
+      target: "https://www.xiteng.com/xitenggamejar/",
       changeOrigin: true,
       secure: false,
       pathRewrite: { "^/api" : "" }
