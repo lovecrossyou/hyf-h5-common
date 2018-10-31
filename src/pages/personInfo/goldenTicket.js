805153a58d7da5b261c5cb43f7b156232434c66b
meng028 <15701664107@163.com>
2018-10-31 17:40:19 +0800

    黄金票

--- src/pages/personInfo/goldenTicket.js
+++ src/pages/personInfo/goldenTicket.js
@@ -2,43 +2,32 @@ import React from 'react';
 
 import { connect } from 'dva';
 import styles from './page.css';
-import {my_gold_bar} from '../../assets/my_gold_bar.png'
+import golden from '../../assets/golden.png'
+import golden_titi from '../../assets/golden_titi.png'
 
 function GoldenTicket(props) {
-  return <div>
-    <div className={styles.xiteng_golden_ticket_container}>
-        <div className={styles.xiteng_golden_ticket}>
-          <div className={styles.my_golden_bar}>
-            <img src={my_gold_bar} alt=""/>
-          </div>
-          <div className={styles.my_gold_bar_info}>
-            <span style={{color:"#cd243e",paddingRight:"10px"}}>100g金条</span>
-            <span>中国黄金 完全免费 零风国黄金 完全免费 零风国黄金 完全免费 零风险</span>
-          </div>
-          <div className={styles.my_gold_bar_info_second}>同步福彩双色球,每周二,四,日揭晓中签,中了就送!</div>
-          <div className={styles.my_gold_bar_info_third}>
-            <span>0元抢金条</span>
-            <span>￥<span className={styles.zero_bigger}>0</span>.00</span>
-            <span>原价:29300.00元</span>
-          </div>
-        </div>
-        <div className={styles.xiteng_golden_ticket_intro}>
-          <div className={styles.xiteng_golden_ticket_intro_left}>
-            {/*<img src="" alt=""/>*/}
-          </div>
-          <div className={styles.xiteng_golden_ticket_intro_right}>
-            <div>黄金票：1张</div>
-            <div>金额：0元</div>
-            <div>揭晓日期：每周二、四、日22:00</div>
-            <div className={styles.xiteng_golden_ticket_intro_righty}>扫描二维码，立即领取</div>
-          </div>
-        </div>
+  const {qrData} = props.personInfo;
+  return <div className={styles.golden_ticket_container}>
+    <div className={styles.golden_ticket_title}>
+      <img src={golden_titi} alt=""/>
+    </div>
+    <img className={styles.golden_ticket_main} src={golden} alt=""/>
+    <div className={styles.xiteng_golden_ticket_intro}>
+      <div className={styles.xiteng_golden_ticket_intro_left}>
+        <img src={qrData} alt=""/>
+      </div>
+      <div className={styles.xiteng_golden_ticket_intro_right}>
+        <div>黄金票：1张</div>
+        <div>金额：0元</div>
+        <div>揭晓日期：每周二、四、日22:00</div>
+        <div className={styles.erweima}>扫面二维码，立即领取</div>
+      </div>
     </div>
   </div>
 }
 
 export default connect(state => {
   return {
-
+    personInfo:state.personInfo
   };
 })(GoldenTicket);
--- src/pages/personInfo/rushTopurchase.js
+++ src/pages/personInfo/rushTopurchase.js
@@ -3,28 +3,33 @@ import React from 'react';
 import { connect } from 'dva';
 import styles from './page.css';
 import { routerRedux } from 'dva/router';
-import {my_gold_bar} from '../../assets/my_gold_bar.png'
-console.log(111111111,my_gold_bar);
 
 function RushTopurchase(props) {
-  return <div className={styles.rush_topurchase_container}>
-    <div className={styles.rush_topurchase_userinfo}>
-      <img className={styles.rush_topurchase_userinfo_touxiang} src={my_gold_bar} alt=""/>
-      <span className={styles.rush_topurchase_userinfo_name}>贝贝</span>
+  const {userInfo} = props.personInfo;
+  if(userInfo==null){
+    return null;
+  }else{
+    const {cnName,icon} = userInfo.userInfo;
+    console.log('11111111111',userInfo)
+    return <div className={styles.rush_topurchase_container}>
+      <div className={styles.rush_topurchase_userinfo}>
+        <img className={styles.rush_topurchase_userinfo_touxiang} src={icon} alt=""/>
+        <span className={styles.rush_topurchase_userinfo_name}>{cnName}</span>
+      </div>
+      <div className={styles.rush_topurchase_gold}>亲，我送你一张喜腾黄金票！</div>
+      <div className={styles.rush_topurchase_gold_info}>凭票0元抢金条、同步福彩双色球，选中6个红色球1个蓝色球即可免费领取金条，完全免费，公开透明，选中即送每周二、四、日22:00揭晓中签</div>
+      <div className={styles.rush_topurchase_gold_info_pic}>
+        {/*<img src={my_gold_bar} alt=""/>*/}
+      </div>
+      <div className={styles.rush_topurchase_gold_btn}>
+        <button>立即领取</button>
+      </div>
     </div>
-    <div className={styles.rush_topurchase_gold}>亲，我送你一张喜腾黄金票！</div>
-    <div className={styles.rush_topurchase_gold_info}>凭票0元抢金条、同步福彩双色球，选中6个红色球1个蓝色球即可免费领取金条，完全免费，公开透明，选中即送每周二、四、日22:00揭晓中签</div>
-    <div className={styles.rush_topurchase_gold_info_pic}>
-      <img src={my_gold_bar} alt=""/>
-    </div>
-    <div className={styles.rush_topurchase_gold_btn}>
-      <button>立即领取</button>
-    </div>
-  </div>
+  }
 }
 
 export default connect(state => {
   return {
-
+    personInfo:state.personInfo
   };
 })(RushTopurchase);
