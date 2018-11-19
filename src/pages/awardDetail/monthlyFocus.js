effe41570dc042e9ff51b5eaa46b6945a9ed8760
meng028 <15701664107@163.com>
2018-11-19 18:16:10 +0800

    x

--- src/pages/awardDetail/monthlyFocus.js
+++ src/pages/awardDetail/monthlyFocus.js
@@ -3,6 +3,8 @@ import { connect } from 'dva';
 import styles from './page.css';
 import DocumentTitle from 'react-document-title';
 import icon_guan from '../../assets/icon_guan@3x.png'
+import icon_ji from '../../assets/icon_ji@3x.png'
+import icon_ya from '../../assets/icon_ya@3x.png'
 
 function MonthlyFocus(props) {
   const {zerogold} = props.store;
@@ -28,7 +30,7 @@ function MonthlyFocus(props) {
               <div className={styles.monthly_focus_section_list_avatar}>
 
               </div>
-              <div>我</div>
+              <div className={styles.monthly_focus_section_list_name}>高阳</div>
             </div>
             <div className={styles.monthly_focus_section_list_portion}><span>1200</span>份</div>
           </div>
@@ -42,10 +44,34 @@ function MonthlyFocus(props) {
                 <div className={styles.monthly_focus_section_list_avatar}>
 
                 </div>
-                <div>我</div>
+                <div className={styles.monthly_focus_section_list_name}>高阳</div>
               </div>
               <div className={styles.monthly_focus_section_list_portion}><span>1200</span>份</div>
             </div>
+          <div className={styles.monthly_focus_section_list_item}>
+            <div className={styles.monthly_focus_section_list_tit_left}>
+              <div className={styles.monthly_focus_section_list_guan}>
+                <img src={icon_ya} alt=""/>
+              </div>
+              <div className={styles.monthly_focus_section_list_avatar}>
+
+              </div>
+              <div className={styles.monthly_focus_section_list_name}>高阳</div>
+            </div>
+            <div className={styles.monthly_focus_section_list_portion}><span>1200</span>份</div>
+          </div>
+          <div className={styles.monthly_focus_section_list_item}>
+            <div className={styles.monthly_focus_section_list_tit_left}>
+              <div className={styles.monthly_focus_section_list_guan}>
+                <img src={icon_ji} alt=""/>
+              </div>
+              <div className={styles.monthly_focus_section_list_avatar}>
+
+              </div>
+              <div className={styles.monthly_focus_section_list_name}>高阳</div>
+            </div>
+            <div className={styles.monthly_focus_section_list_portion}><span>1200</span>份</div>
+          </div>
         </div>
       </div>
     </div>
