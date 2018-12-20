56e362136c61de99a185e067e23ed656e27d628d
xiannvrong <1796940658@qq.com>
2018-12-20 10:18:11 +0800

    分类商品详情

--- src/utils/authority.js
+++ src/utils/authority.js
@@ -1,14 +1,14 @@
 import config from './config';
 
 export function getAccessToken() {
-  // return {"app_key":"b5958b665e0b4d8cae77d28e1ad3f521","signature":"BD60133D4A74C4926FD592E8DD85BE94","access_token":"3550f4cf636a48d3b91417cbca29f851"}
-  // return {
-  //   access_token:"d493f7c69397413f88d641c145d49fa4",
-  //   app_key:"b5958b665e0b4d8cae77d28e1ad3f521",
-  //   signature:"06CF602ECEEB68EB308111759091DF9C"
-  // }
-  //test
-  // return {"app_key":"b5958b665e0b4d8cae77d28e1ad3f521","signature":"63FE2FE8D69F805D149E3D3029921508","access_token":"935e84d786ef44cfa8215ca18e6d2ddf"}
+  return {"app_key":"b5958b665e0b4d8cae77d28e1ad3f521","signature":"BD60133D4A74C4926FD592E8DD85BE94","access_token":"3550f4cf636a48d3b91417cbca29f851"}
+  return {
+    access_token:"d493f7c69397413f88d641c145d49fa4",
+    app_key:"b5958b665e0b4d8cae77d28e1ad3f521",
+    signature:"06CF602ECEEB68EB308111759091DF9C"
+  }
+  test
+  return {"app_key":"b5958b665e0b4d8cae77d28e1ad3f521","signature":"63FE2FE8D69F805D149E3D3029921508","access_token":"935e84d786ef44cfa8215ca18e6d2ddf"}
   return JSON.parse(localStorage.getItem('accessToken'));
 }
 
