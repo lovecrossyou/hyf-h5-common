ac03473490392bf905edf69e0129312155f2fcf1
love1234 <345841634@qq.com>
2018-12-20 10:28:12 +0800

    xx

--- .umirc.js
+++ .umirc.js
@@ -17,7 +17,6 @@ export default {
   proxy: {
     "/api": {
       // target: "http://123.57.161.212:9939/xitenggamejar/",
-      // "target": "http://192.168.1.83:9939/xitenggamejar/",
       target: "https://www.xiteng.com/xitenggamejar/",
       changeOrigin: true,
       secure: false,
