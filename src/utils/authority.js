65b9ef813e0ffd195455c18c39b740ac02c4bfbc
love1234 <345841634@qq.com>
2019-02-01 16:22:02 +0800

    xx

--- src/utils/authority.js
+++ src/utils/authority.js
@@ -1,7 +1,7 @@
 import config from './config';
 
 export function getAccessToken() {
-  return {"app_key":"b5958b665e0b4d8cae77d28e1ad3f521","signature":"BD60133D4A74C4926FD592E8DD85BE94","access_token":"3550f4cf636a48d3b91417cbca29f851"}
+  // return {"app_key":"b5958b665e0b4d8cae77d28e1ad3f521","signature":"BD60133D4A74C4926FD592E8DD85BE94","access_token":"3550f4cf636a48d3b91417cbca29f851"}
   // return {
   //   access_token:"d493f7c69397413f88d641c145d49fa4",
   //   app_key:"b5958b665e0b4d8cae77d28e1ad3f521",
