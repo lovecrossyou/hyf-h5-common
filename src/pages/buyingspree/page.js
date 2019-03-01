77234a12ad7af8277a2690c339b3648906489641
love1234 <345841634@qq.com>
2019-03-01 17:11:48 +0800

    xxx

--- src/pages/buyingspree/page.js
+++ src/pages/buyingspree/page.js
@@ -3,8 +3,7 @@ import DocumentTitle from 'react-document-title';
 import { connect } from 'dva';
 import styles from './page.css';
 import {ActivityIndicator} from "../../components/ActivityIndicator";
-import { routerRedux } from 'dva/router';
-import photo_shengdan from '../../assets/photo_shengdan@3x.png'
+import photo_shengdan from '../../assets/photo_shengdan@3x.jpg'
 import xiteng from '../../assets/xiteng.jpg'
 
 function BuyingSpree(props){
@@ -20,7 +19,7 @@ function BuyingSpree(props){
               <img src={xiteng} alt=""/>
             </div>
             <div className={styles.buying_spree_section_name}>喜腾</div>
-            <div className={styles.buying_spree_section_content}>喜腾送你的圣诞礼物，免费抽签抢100g金条！每周二、周四、周日22:00揭晓中签，选中双色球就送，下载喜腾抢购APP即可免费参与，赶紧去下载吧！</div>
+            <div className={styles.buying_spree_section_content}>猪年大吉,金猪送福,抽签抢金条,100g,抽中即送,公开透明。每周二、四、日22:00揭晓中签,选中双色球就送,下载喜腾抢购APP即可免费参与,赶紧去下载吧!</div>
             <div className={styles.buying_spree_section_img}>
               <img src={photo_shengdan} alt=""/>
             </div>
